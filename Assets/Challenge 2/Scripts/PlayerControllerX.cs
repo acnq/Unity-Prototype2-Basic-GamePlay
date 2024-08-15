ac0f503ed25946ba29c4962ee7c4419acb88ccd3
acnq <fanyuanhaotc@163.com>
2024-08-15 23:37:40 +0300

    content

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float coolDown = 0; // used to avoid user to press "Space" constantly.
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coolDown > 0)
+        {
+            coolDown -= Time.deltaTime;
+        }
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDown <= 0)
+        {
+            coolDown = 0.5f;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }       
+    }
+}
